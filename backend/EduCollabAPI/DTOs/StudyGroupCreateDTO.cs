8ebc707406c73176a460f9250a575280a975fb94
hanya <haniaaamrr@gmail.com>
2026-05-07 02:44:45 +0300

    DTOs

---
+++ backend/EduCollabAPI/DTOs/StudyGroupCreateDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduCollabAPI.DTOs
+{
+    public class StudyGroupCreateDTO
+    {
+        [Required]
+        public int CreatorId { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(2, 100)]
+        public int MaxMembers { get; set; }
+
+        [Required]
+        public string MeetingType { get; set; }
+
+        [Required]
+        public string MeetingSchedule { get; set; }
+    }
+}
