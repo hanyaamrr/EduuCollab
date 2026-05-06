fa5958db2de42126279da110b9a385f78c205fd0
farah-samy <farahsami12@gmail.com>
2026-05-07 00:27:31 +0300

    notifications

--- backend/EduCollabAPI/Program.cs
+++ backend/EduCollabAPI/Program.cs
@@ -1,9 +1,29 @@
+using EduCollabAPI.Data;
+using EduCollabAPI.Hubs;
+using EduCollabAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(Options =>
+Options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddSignalR();
+
+builder.Services.AddScoped<NotificationService>();
+
+builder.Services.AddHostedService<MeetingReminderService>();
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+app.MapControllers();
+
+app.MapHub<NotificationHub>("/hubs/notifications");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
