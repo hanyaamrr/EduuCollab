f5e785b6736780eac5ab0281b4c12fc7d11e6ba6
hanya <haniaaamrr@gmail.com>
2026-05-07 01:36:55 +0300

    studygroup model

--- backend/EduCollabAPI/Models/StudyGroup.cs
+++ backend/EduCollabAPI/Models/StudyGroup.cs
@@ -1,6 +1,27 @@
-﻿namespace EduCollabAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduCollabAPI.Models
 {
     public class StudyGroup
     {
+        [Key]
+        public int Id { get; set; }
+        public string CreatorName { get; set; }
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public string Subject { get; set; } 
+        public string Location { get; set; }
+        public int MaxMembers { get; set; }
+        public string MeetingType { get; set; }  //online or offline
+        public string MeetingSchedule { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("CreatorId")]
+        public int CreatorId { get; set; }
+
+        public List<Material> GroupMaterials { get; set; } = new List<Material>();
+        public List<GroupMember> Members { get; set; } = new List<GroupMember>();
+        //public List<Discussion> Discussions { get; set; } = new List<Discussion>();
     }
 }
